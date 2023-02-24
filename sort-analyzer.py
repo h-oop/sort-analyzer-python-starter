6c2484707a6f33f6b5cd9f81bbab4d493171ef90
Abby Doris <a.doris@EDU.epsb.ca>
2023-02-24 14:38:59 -0700

    asdf

--- sort-analyzer.py
+++ sort-analyzer.py
@@ -17,6 +17,64 @@ def loadDataArray(fileName):
     return temp
 
 
+def bubble_sort(array):
+    start = time.time()
+    for i in range(len(array)):
+
+        for j in range(len(array)-i-1):
+
+            if array[j] > array[j+1]:
+
+                array[j], array[j+1] = array[j+1], array[j]
+    
+    end = time.time()
+    timeel = end - start
+    print(timeel)
+
+
+def selection_sort(array):
+    start = time.time()
+    for i in range(len(array)-1):
+
+        #get new minimum
+        min_index = i
+
+        for j in range(i+1, len(array)):
+
+            if array[j] < array[min_index]:
+                min_index = j
+
+        array[i], array[min_index] = array[min_index], array[i]
+
+    end = time.time()
+    timeel = end - start
+    print(timeel)
+
+
+def insertion_sort(array):
+    start = time.time()
+    for i in range(1, len(array)):
+
+        insert_pos = i
+        insert_val = array[insert_pos]
+
+        #     is the position    is the item to the left of the 
+        #     bigger than 0?      position bigger?
+        while insert_pos > 0 and array[insert_pos - 1] > insert_val:
+            
+            array[insert_pos-1], array[insert_pos] = array[insert_pos], array[insert_pos-1]
+
+            insert_pos -=1
+
+    end = time.time()
+    timeel = end - start
+    print(timeel)
+
+
+
+
+
+
 # LOAD DATA FILE INTO GLOBAL VARIABLES
 randomData = loadDataArray("data-files/random-values.txt")
 reversedData = loadDataArray("data-files/reversed-values.txt")
